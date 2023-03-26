d587ba46342e4d5979d1deca199d38a7562d0c3c
Er Riyaz Ahmad Wani <74089872+erriyazwani@users.noreply.github.com>
2023-03-26 17:25:38 +0530

    Update create a bottom_navigation_with_fragment.java

--- create a bottom_navigation_with_fragment.java
+++ create a bottom_navigation_with_fragment.java
@@ -70,3 +70,17 @@
             return false;
         });
 
+
+
+// someextra thing to change ui in fragment
+
+@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+
+        myTextView=view.findViewById(R.id.mytextview);
+        myTextView.setText("Got it !");
+        return view;
+    }
